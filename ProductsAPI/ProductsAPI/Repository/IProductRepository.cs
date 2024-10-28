82cc2166aa426d21ca1583cd06077815d25e9509
Aman Mohammed P <84070886+amxn-mhd@users.noreply.github.com>
2024-10-28 10:33:51 +0530

    Add files via upload

---
+++ ProductsAPI/ProductsAPI/Repository/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using ProductsAPI.Models;
+
+namespace ProductsAPI.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+
+        Product GetProductByID(int product);
+
+        void InsertProduct(Product product);
+
+        void DeleteProduct(int productId);
+
+        void UpdateProduct(Product product);
+
+        void Save();
+    }
+}
