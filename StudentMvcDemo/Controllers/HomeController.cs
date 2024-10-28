4c918ebd7dc9ddbc2b21f98391f26f92aa1eb1f9
Aman Mohammed P <84070886+amxn-mhd@users.noreply.github.com>
2024-10-28 10:35:48 +0530

    Add files via upload

---
+++ StudentMvcDemo/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using StudentMvcDemo.Models;
+using System.Diagnostics;
+
+namespace StudentMvcDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
